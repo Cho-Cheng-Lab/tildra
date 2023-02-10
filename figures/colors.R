3a7fd223fabdd8f1d851cd2a56833f8694bfadfd
David Wu <symbiologist@gmail.com>
2023-02-09 22:25:31 -0800

    Update figs

--- figures/colors.R
+++ figures/colors.R
@@ -1,2 +1,4 @@
 colors_response2 <- c('dodgerblue4', 'orangered') %>% alpha(alpha = 0.8)
 colors_response2 <- ggthemes::few_pal()(2)
+
+colors_carto <- rcartocolor::carto_pal(n = 12, name = 'Prism')
