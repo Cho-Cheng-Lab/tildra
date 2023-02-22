55009e7aaf9b3f28a1baccccec3fdc78c1b86d1a
David Wu <symbiologist@gmail.com>
2023-02-21 20:48:43 -0800

    Polar sector plot

--- figures/colors.R
+++ figures/colors.R
@@ -1,5 +1,6 @@
 colors_response2 <- c('dodgerblue4', 'orangered') %>% alpha(alpha = 0.8)
 colors_response2 <- ggthemes::few_pal()(2)
 colors_response3 <- ggthemes::few_pal()(3) %>% rev()
+colors_response3 <- c('#1446A0', '#127475', '#FCAF58')
 
 colors_carto <- rcartocolor::carto_pal(n = 12, name = 'Prism')
